9777fd4ac41fd3216a11a4070e26379fd33135f4
Dry <dryt1301@gmail.com>
2023-05-30 21:00:43 +0900

    Add files via upload
    this_code_including_error

---
+++ test_ai/test_ai/test_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_aiApp.swift
+//  test_ai
+//
+//  Created by 洞井僚太 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct test_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test_ai/test_aiUITests/test_aiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test_aiUITestsLaunchTests.swift
+//  test_aiUITests
+//
+//  Created by 洞井僚太 on 2023/03/25.
+//
+
+import XCTest
+
+final class test_aiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
